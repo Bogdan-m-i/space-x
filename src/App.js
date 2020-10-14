e4b5679276697ca324af392e56942ecbb1a0a7e1
Bogdan <bogdan.m.i@yandex.ru>
2020-10-14 21:15:04 +0500

    space-x v 1.0.0

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import Header from './components/Header/header.js';
+import Main from './components/Main/main.js';
+import Features from './components/Features/features.js';
+import Footer from './components/Footer/footer.js';
+import Details from './components/Details/deatils.js';
+import Calendar from './components/Calendar/calendar.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Header />
+  <Main />
+  <Features />
+  <Calendar />
+  <Footer />
+  <Details />
+  </>
   );
 }
 
